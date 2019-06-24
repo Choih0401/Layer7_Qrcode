c159ab2da5e25bfb3f8b8b045f4f1473468f81e0
Choih0401 <danielch0401@naver.com>
2019-06-24 15:31:27 +0900

    update app.py error

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.secret_key = 'why would I tell you my secret key?'
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'sunrin'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'ch04010508'
 app.config['MYSQL_DATABASE_DB'] = 'userlist'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
@@ -88,14 +88,13 @@ def score():
         data = cursor.fetchall()
 
         con.commit()
+        cursor.close()
+        con.close()
         return render_template('score.html', score = str(data[0]).split(',')[0].split('(')[1])
 
 
     except Exception as e:
         return render_template('error.html',error = str(e))
-    finally:
-        cursor.close()
-        con.close()
 
 @app.route('/signUp',methods=['POST','GET'])
 def signUp():
@@ -117,17 +116,22 @@ def signUp():
 
             if len(data) is 0:
                 conn.commit()
+                cursor.close()
+                conn.close()
                 return render_template('error.html', error = 'Success')
             else:
+                cursor.close()
+                conn.close()
                 return render_template('error.html', error = str(data[0]).split('\'')[1].split('\'')[0])
+
         else:
+            cursor.close()
+            conn.close()
             return render_template('error.html', error = 'Required Fileds')
 
     except Exception as e:
         return render_template('error.html', error = str(e))
-    finally:
-        cursor.close()
-        conn.close()
+
 
 
 @app.route('/answerSubmit',methods=['POST','GET'])
@@ -144,23 +148,21 @@ def answerSubmit():
         cursor = conn.cursor()
         cursor.callproc('sp_submit',(_uid,_time,_flag))
         data = cursor.fetchall()
+        conn.commit()
+        cursor.close()
+        conn.close()
         conn2 = mysql.connect()
         cursor2 = conn2.cursor()
         cursor2.callproc('sp_result',(_uid,_flag))
         data2 = cursor2.fetchall()
-
-        conn.commit()
         conn2.commit()
+        cursor2.close()
+        conn2.close()
         return render_template('result.html', error = str(data2[0]).split('\'')[1].split('\'')[0])
 
     except Exception as e:
         return render_template('result.html', error = str(e))
-    finally:
-        cursor.close()
-        conn.close()
-        cursor2.close()
-        conn2.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
